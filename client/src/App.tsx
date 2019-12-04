d218415a91c90fb026115aee6dbfd8086d2bd619
jpskgc <jpskgc.v3@gmail.com>
2019-12-04 23:38:02 +0900

    add menu bar

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,9 +1,11 @@
 import React, {FC} from 'react';
 import {Route, Switch} from 'react-router';
 import Demo from './components/Demo';
+import Bar from './components/Bar';
 
 const App: FC = () => (
   <>
+    <Bar />
     <Switch>
       <Route path="/" exact component={Demo} />
     </Switch>
