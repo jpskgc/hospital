2b76d378995e9cad672c5b34ee8e36ef585f9228
jpskgc <jpskgc.v3@gmail.com>
2019-12-08 08:48:16 +0900

    add category

---
+++ client/src/components/Detail/CategoryGrid.tsx
@@ -0,0 +1,75 @@
+import React from 'react';
+import {Container} from '@material-ui/core';
+import Typography from '@material-ui/core/Typography';
+import {makeStyles, createStyles, Theme} from '@material-ui/core/styles';
+import Paper from '@material-ui/core/Paper';
+import Grid from '@material-ui/core/Grid';
+import {
+  FaStethoscope,
+  FaHeartbeat,
+  FaRegHospital,
+  FaUserNurse,
+  FaClinicMedical,
+} from 'react-icons/fa';
+
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    root: {
+      flexGrow: 1,
+      margin: 10,
+    },
+    paper: {
+      padding: theme.spacing(2),
+      textAlign: 'left',
+      color: theme.palette.text.secondary,
+    },
+  })
+);
+
+export default function CategoryGrid() {
+  const classes = useStyles();
+  return (
+    <Container maxWidth="md">
+      <Typography>カテゴリ別の患者クチコミ</Typography>
+      <div className={classes.root}>
+        <Grid container spacing={1}>
+          <Grid item xs={2}>
+            <Paper className={classes.paper}>
+              <Typography>
+                <FaStethoscope /> 診断説明
+              </Typography>
+            </Paper>
+          </Grid>
+          <Grid item xs={2}>
+            <Paper className={classes.paper}>
+              <Typography>
+                <FaHeartbeat /> 手術の腕
+              </Typography>
+            </Paper>
+          </Grid>
+          <Grid item xs={2}>
+            <Paper className={classes.paper}>
+              <Typography>
+                <FaRegHospital /> 入院施設
+              </Typography>
+            </Paper>
+          </Grid>
+          <Grid item xs={2}>
+            <Paper className={classes.paper}>
+              <Typography>
+                <FaUserNurse /> 看護ケア
+              </Typography>
+            </Paper>
+          </Grid>
+          <Grid item xs={2}>
+            <Paper className={classes.paper}>
+              <Typography>
+                <FaClinicMedical /> 退院後ケア
+              </Typography>
+            </Paper>
+          </Grid>
+        </Grid>
+      </div>
+    </Container>
+  );
+}
--- client/src/components/Detail/Detail.tsx
+++ client/src/components/Detail/Detail.tsx
@@ -2,12 +2,14 @@ import React, {FC} from 'react';
 import Header from './Header';
 import Tab from './DetailTab';
 import Chart from './Chart';
+import CategoryGrid from './CategoryGrid';
 
 const App: FC = () => (
   <>
     <Header />
     <Tab />
     <Chart />
+    <CategoryGrid />
   </>
 );
 
