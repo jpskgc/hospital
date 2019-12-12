3fc69378047348a2613bb41245759f2a1b49401f
jpskgc <jpskgc.v3@gmail.com>
2019-12-12 11:17:30 +0900

    fix search in top header

--- client/src/components/Top/Header.tsx
+++ client/src/components/Top/Header.tsx
@@ -1,9 +1,9 @@
 import React from 'react';
 import Typography from '@material-ui/core/Typography';
-import {makeStyles} from '@material-ui/core/styles';
+import {fade, makeStyles} from '@material-ui/core/styles';
 import Container from '@material-ui/core/Container';
-import Grid from '@material-ui/core/Grid';
-import Button from '@material-ui/core/Button';
+import SearchIcon from '@material-ui/icons/Search';
+import InputBase from '@material-ui/core/InputBase';
 
 const useStyles = makeStyles(theme => ({
   head: {
@@ -14,6 +14,46 @@ const useStyles = makeStyles(theme => ({
     backgroundColor: '#3f51b5',
     padding: theme.spacing(8, 0, 6),
     color: theme.palette.common.white,
+  },
+  search: {
+    position: 'relative',
+    borderRadius: theme.shape.borderRadius,
+    backgroundColor: fade(theme.palette.common.white, 0.15),
+    '&:hover': {
+      backgroundColor: fade(theme.palette.common.white, 0.25),
+    },
+    marginRight: theme.spacing(2),
+    marginLeft: 0,
+    width: '100%',
+    [theme.breakpoints.up('sm')]: {
+      marginLeft: theme.spacing(3),
+      width: 'auto',
+    },
+    marginTop: 36,
+    height: 48,
+  },
+  searchIcon: {
+    width: theme.spacing(7),
+    height: '100%',
+    position: 'absolute',
+    pointerEvents: 'none',
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  inputRoot: {
+    color: 'inherit',
+    height: 48,
+    width: '100%',
+  },
+  inputInput: {
+    padding: theme.spacing(1, 1, 1, 7),
+    transition: theme.transitions.create('width'),
+    width: '100%',
+    [theme.breakpoints.up('md')]: {
+      width: 'auto',
+    },
+    height: 48,
   },
 }));
 
@@ -32,7 +72,21 @@ export default function Header() {
           >
             「入院」のすべてを、オープンに。
           </Typography>
-          <div></div>
+          <Container maxWidth="sm">
+            <div className={classes.search}>
+              <div className={classes.searchIcon}>
+                <SearchIcon />
+              </div>
+              <InputBase
+                placeholder="病院名で検索する"
+                classes={{
+                  root: classes.inputRoot,
+                  input: classes.inputInput,
+                }}
+                inputProps={{'aria-label': 'search'}}
+              />
+            </div>
+          </Container>
         </Container>
       </div>
     </>
